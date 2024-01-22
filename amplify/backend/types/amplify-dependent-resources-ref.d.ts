0027cd5b4546a0740b783e89ff655f504544d789
root <root@ip-10-0-1-48.ap-northeast-1.compute.internal>
2024-01-22 07:30:41 +0000

    changed auth source

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "takenihtest1cca74524": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
